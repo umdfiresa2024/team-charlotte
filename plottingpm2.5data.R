e13b5e26d1e14fb09e02f5abf80beed0c9a93cce
davidjguan <dguan@umd.edu>
2024-07-04 22:01:09 -0400

    finished demographic stuff for income, and calculated that on the individual level, the benefit of pollution reduction is experienced the same across different income groups

--- plottingpm2.5data.R
+++ plottingpm2.5data.R
@@ -11,13 +11,15 @@ alldata <- read.csv("refined_date_PM25_daily_data.csv")
 testdata <- data %>% filter(city_num == 1)
 testdata2 <- data %>% filter(city_num == 2)
 
-#all the stations pm2.5 over time with points
-ggplot(data = alldata,aes(x = date, y=pm25,color = city_num)) +labs(y= "PM2.5", x = "Date")+ geom_point()+facet_wrap(~ city_num)
+#tried plotting over time but having each day as a point was too crowded
+# ggplot(data = alldata,aes(x = date, y=pm25,color = city_num)) +labs(y= "PM2.5", x = "Date")+ geom_point()+facet_wrap(~ city_num)
 
-#trying with box plot like rygel said
-ggplot(data = alldata,aes(x = date, y=pm25,color = city_num)) +labs(y= "PM2.5", x = "Date")+ geom_boxplot()+facet_wrap(~ city_num)
 
-ggplot(data = testdata,aes(x = date, y=pm25)) + geom_point()
+ggplot(data = alldata,aes(x = city_num, y=pm25,color = city_num)) +labs(y= "PM2.5", x = "Station Number")+ geom_boxplot(aes(group = city_num, color = factor(city_num)),outlier.shape = NA)
 
-ggplot(data = testdata2,aes(x = date, y=pm25)) + geom_point()
+
+#individual pm2.5 over time plots for some specific light rail stations (too crowded)
+#ggplot(data = testdata,aes(x = date, y=pm25)) + geom_point(shape = "o")
+
+#ggplot(data = testdata2,aes(x = date, y=pm25)) + geom_point()
 
